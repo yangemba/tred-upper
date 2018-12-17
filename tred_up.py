652db1b4f0edebe687d726262d368b313ef93d57
yangemba <yangemba@gmail.com>
2018-12-17 17:38:45 +0200

    controlv1

--- tred_up.py
+++ tred_up.py
@@ -1,16 +1,43 @@
 import requests
 import bs4
+import random
+import time
 
-response = requests.get("https://2ch.hk/ch")
-soup = bs4.BeautifulSoup(response.text, "html.parser")
-search_area = soup.findAll("div", {"class": "thread"})
+wait = random.randrange(7200, 18001, 3600)
+# print(wait)
 
-#print(search_area[1])
+with open('const.txt', 'r') as file:
+    file.seek(0)
+    CONST = file.read()
+# print("const = ", CONST)
 
-CONST = "thread-63521"
+with open('dict.txt', 'r') as file:
+    file.seek(0)
+    text = file.read()
+    dict = text.split(',')
+# print("dict = ", dict)
 
-if CONST in str(search_area[4]):
-    print("yes")
-    response = requests.get("https://2ch.hk/ch/res/63521.html")
-    if response:
-        print("connection")
+last_dict_element = (len(dict) - 1)
+len_array = random.randint(0,last_dict_element)
+BUMP_WORD = dict[len_array]
+# print("Bump-word = ", BUMP_WORD)
+
+while True:
+    response = requests.get("https://2ch.hk/ch")
+
+    if response.status_code == 200:
+        soup = bs4.BeautifulSoup(response.text, "html.parser")
+        search_area = soup.findAll("div", {"class": "thread"})
+
+        if CONST not in str(search_area[1]):
+            print("connection")
+            response = requests.get("https://2ch.hk/ch/res/63521.html")
+
+            if response:
+                print("sent bump")
+                response = requests.post("https://2ch.hk/ch/res/63521.html", data = BUMP_WORD)
+        else:
+            print("Its already bumped")
+    else:
+        continue
+    time.sleep(wait)
